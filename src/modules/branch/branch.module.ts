29e2c39c46c3e849513aa30092a038a13c1e6d2f
Truc Nguyen <truc95520@gmail.com>
2025-10-06 21:47:11 +0700

    fix: add token decorator to api branch

--- src/modules/branch/branch.module.ts
+++ src/modules/branch/branch.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Branch } from '@shared/db/entities/branch.entity';
 import { BranchController } from './branch.controller';
 import { BranchService } from './branch.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Branch])],
   controllers: [BranchController],
   providers: [BranchService],
   exports: [BranchService]
