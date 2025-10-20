b4aefbbbe620c4b4b8e1975178f156aab864278c
letrungphong <ojt.phong.le@napaglobal.com>
2025-10-20 23:27:25 +0700

    fix: convert error is current of refreshment

--- src/modules/refreshments/dto/create-refreshment.dto.ts
+++ src/modules/refreshments/dto/create-refreshment.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Type } from 'class-transformer';
+import { Transform, Type } from 'class-transformer';
 import { IsBoolean, IsInt, IsNotEmpty, IsString, Min, MinLength } from 'class-validator';
 
 export class CreateRefreshmentDto {
@@ -27,7 +27,13 @@ export class CreateRefreshmentDto {
     description: 'Indicates if the refreshment is currently available',
     default: true
   })
-  @Type(() => Boolean)
+  @Type(() => String) //  ép luôn về string trước
+  @Transform(({ value }) => {
+    const v = String(value).toLowerCase().trim();
+    if (v === 'false' || v === '0') return false;
+    if (v === 'true' || v === '1') return true;
+    return true;
+  })
   @IsBoolean()
   isCurrent: boolean = true;
 }
