974897b95a118f4a2567336b2c5c35da80c4d9d5
thanhtu <thanhthanhvava2004@gmail.com>
2025-10-27 19:38:07 +0700

    feat: add refresh token in response

--- src/modules/auth/interfaces/authResponse.interface.ts
+++ src/modules/auth/interfaces/authResponse.interface.ts
@@ -2,10 +2,12 @@ import { AccountResponseDto } from '@modules/accounts/dto/account-response.dto';
 
 export interface LoginResponse {
   accessToken?: string;
+  refreshToken?: string;
   message?: string;
   account: AccountResponseDto;
 }
 
-export interface RefreshTokenResponse {
+export interface AuthTokens {
   accessToken: string;
+  refreshToken: string;
 }
